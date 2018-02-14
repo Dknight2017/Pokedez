43026c8b5767f16082b44db874972d957530b572
Dylan Knight <samuraiknight713@gmail.com>
2018-02-13 18:34:30 -0800

    Pokemon Details

--- src/PokemonList.js
+++ src/PokemonList.js
@@ -1,3 +1,40 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-export default Pokemons 
+class Pokemons extends React.Component {
+
+  state = {
+    filterSting: ''
+  }
+
+updateFilter() {
+    this.setState ({
+      filterString: this.refs.filter.value
+    })
+}
+
+render () {
+    const { filterString } = this.state
+    const pokemons = this.props.pokemons.filter((p) => {
+        return p.title.toUpperCase().includes(filterString.toUpperCase())
+    })
+
+    return (
+        <div className="Pokemons">
+          <input onChange={this.updateFilter.bind(this)} ref="filterinput" className="input is-large" type="text" placeholder="Filter The List" />
+          <ul className="menu-list">
+            {books.map((p) => {
+              return (
+                <li key={p._id}>
+                  <Link to={`/pokemons/${p._id}`}>{p.title}</Link>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+      )
+    }
+  }
+  
+  export default Pokemons 
+
