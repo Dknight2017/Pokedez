01615de0cfc9852754d6c3bad36c57915c768961
Dylan Knight <samuraiknight713@gmail.com>
2018-02-13 18:05:04 -0800

    First-Day

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+
+
+class App extends Component {
+
+  // state = {
+  //   pokemons = pokemonlist
+  // }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Gotta catchem all!!</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
